c5a5333ab3bdf516758f7302b510fa505539e693
Kun Young Oh <andy.k.oh@hotmail.com>
2020-02-03 22:04:29 +0000

    template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,6 @@
 const assertEqual = function(actual, expected) {
-  return (actual === expected) ? ("✔✔✔ Assertion Passed: " + actual + " === " + expected) : ("💥💥💥 Assertion Failed: " + actual + " !== " + expected);
+  return (actual === expected) ? (`✔✔✔ Assertion Passed: ${actual} === ${expected}`) : (`💥💥💥 Assertion Failed: ${actual} !== ${expected}`);
 };
 
+console.log(assertEqual("da", "da"));
 console.log(assertEqual("da", "d"));
