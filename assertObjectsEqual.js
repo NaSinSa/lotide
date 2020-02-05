00121563cfa3198407581bd9f5d84025f7a3be80
NaSinSa <andy.k.oh@hotmail.com>
2020-02-05 21:01:40 +0000

    showing comp result

---
+++ assertObjectsEqual.js
@@ -0,0 +1,30 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (let key in object1) {
+    if (Array.isArray(object1[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  console.log(eqObjects(actual, expected) ? (`✔✔✔ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`) : (`💥💥💥 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`));
+};
+
+assertObjectsEqual({a: '1', b: 2}, {b: 2, a: 1})
