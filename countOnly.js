c996dcc7ff7dcc5e683dfa1ac27c978b7a692230
Kun Young Oh <andy.k.oh@hotmail.com>
2020-02-05 17:47:49 +0000

    counting

---
+++ countOnly.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  return (actual === expected) ? (`✔✔✔ Assertion Passed: ${actual} === ${expected}`) : (`💥💥💥 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  let countedItems = {};
+  for (const ele of allItems) {
+    if (itemsToCount[ele] && !countedItems[ele]) {
+      countedItems[ele] = 1;
+    } else if (itemsToCount[ele] && countedItems[ele]) {
+      countedItems[ele] += 1;
+    }
+  }
+  return countedItems;
+};
