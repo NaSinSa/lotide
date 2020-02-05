b9fb14722b24d9965ecb53e11445adcf9cad7092
NaSinSa <andy.k.oh@hotmail.com>
2020-02-05 19:14:10 +0000

    find the first key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  return (actual === expected) ? (`✔✔✔ Assertion Passed: ${actual} === ${expected}`) : (`💥💥💥 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = function(obj, value) {
+  for (let key in obj) {
+    if (obj[key] === value) return key; 
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
