51706fdc604405193edceb6bd51093c11652e9fd
NaSinSa <andy.k.oh@hotmail.com>
2020-02-07 16:04:20 -0500

    nested arr feature added

--- eqArrays.js
+++ eqArrays.js
@@ -5,7 +5,9 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
+    if (Array.isArray(arr1[i])) {
+      return eqArrays(arr1[i], arr2[i]);
+    } else if (arr1[i] !== arr2[i]) return false;
   }
   return true;
 }
