31c2a14a4d79b0ee00a90a47c7f0c60f301e94a8
NaSinSa <andy.k.oh@hotmail.com>
2020-02-10 14:40:34 -0500

    deepEqual and notDeepEqual features

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,6 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
 assertArraysEqual(middle([1]), []) // passed
 assertArraysEqual(middle([1, 2]), [])  // passed
@@ -8,3 +9,18 @@ assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])  // passed
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])  // passed
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])  // passed
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 5])  // fail
+
+describe('#middle', () => {
+  it('should return [] when there is only one value in a given array', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it('should return [2] when there are 1, 2, and 3 in a given array', () => {
+    assert.deepEqual(middle([1, 2 ,3]), [2]);
+  });
+  it('should return [3, 4] when a given array [1, 2, 3, 4, 5, 6]', () => {
+    assert.deepEqual(middle([1, 2 ,3, 4, 5, 6]), [3, 4]);
+  });
+  it('should return [3, 4] when a given array [1, 2, 3, 4, 5, 6]', () => {
+    assert.notDeepEqual(middle([1, 2 ,3, 4, 5, 6]), [3, 5]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,16 @@
 const assertEqual = require("../assertEqual");
 const tail = require("../tail");
+const assert = require('chai').assert;
 
 const words = [];
 console.log(tail(words));
 console.log(assertEqual(words.length, 0));
+
+describe("#tail", () => {
+  it("should return 'goodbye' and 'See ya later'", () => {
+    assert.deepEqual(tail(["hello", "goodbye", "See ya later"]), ["goodbye", "See ya later"]);
+  });
+  it("should return []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
